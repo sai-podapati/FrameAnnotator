53e1a4db1c737d4db56c1275c45bbd59e95bc343
sai-podapati <105712881+sai-podapati@users.noreply.github.com>
2024-11-06 23:59:40 +0530

    Create Config.java

---
+++ src/main/java/com/image/Config.java
@@ -0,0 +1,15 @@
+package com.image;
+import java.util.Map;
+
+public class Config {
+    private Map<String, String> categoryColors;
+
+    // Getter and Setter for categoryColors
+    public Map<String, String> getCategoryColors() {
+        return categoryColors;
+    }
+
+    public void setCategoryColors(Map<String, String> categoryColors) {
+        this.categoryColors = categoryColors;
+    }
+}
